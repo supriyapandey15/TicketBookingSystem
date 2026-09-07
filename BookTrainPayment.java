bdc008917612fc8d79ac0e1a586b6d0fd9e16193
Supriya Pandey <113895235+supriyapandey15@users.noreply.github.com>
2026-09-07 13:16:34 +0530

    Update BookTrainPayment.java

--- BookTrainPayment.java
+++ BookTrainPayment.java
@@ -1,4 +1,4 @@
-package com.shashi.servlets;
+package com.supriya.servlets;
 
 import java.io.IOException;
 
@@ -10,8 +10,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.shashi.constant.UserRole;
-import com.shashi.utility.TrainUtil;
+import com.supriya.constant.UserRole;
+import com.supriya.utility.TrainUtil;
 
 @WebServlet("/payment")
 public class BookTrainPayment extends HttpServlet {
