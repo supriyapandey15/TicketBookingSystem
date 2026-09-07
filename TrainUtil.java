efa2e39001a43a05148c9f0c2f292022e0235a08
Supriya Pandey <113895235+supriyapandey15@users.noreply.github.com>
2026-09-07 12:50:51 +0530

    Change package name from com.shashi to com.supriya

--- TrainUtil.java
+++ TrainUtil.java
@@ -1,4 +1,4 @@
-package com.shashi.utility;
+package com.supriya.utility;
 
 import java.util.Arrays;
 import java.util.Optional;
@@ -8,12 +8,12 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.shashi.beans.TrainException;
-import com.shashi.beans.UserBean;
-import com.shashi.constant.ResponseCode;
-import com.shashi.constant.UserRole;
-import com.shashi.service.UserService;
-import com.shashi.service.impl.UserServiceImpl;
+import com.supriya.beans.TrainException;
+import com.supriya.beans.UserBean;
+import com.supriya.constant.ResponseCode;
+import com.supriya.constant.UserRole;
+import com.supriya.service.UserService;
+import com.supriya.service.impl.UserServiceImpl;
 
 public class TrainUtil {
 
