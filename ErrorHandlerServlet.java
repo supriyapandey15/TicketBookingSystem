5d83340a16b8d5652d620061a9b1a4efd224dd2a
Supriya Pandey <113895235+supriyapandey15@users.noreply.github.com>
2026-09-07 13:21:48 +0530

    Update ErrorHandlerServlet.java

--- ErrorHandlerServlet.java
+++ ErrorHandlerServlet.java
@@ -1,4 +1,4 @@
-package com.shashi.servlets;
+package com.supriya.servlets;
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -10,8 +10,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.shashi.beans.TrainException;
-import com.shashi.constant.ResponseCode;
+import com.supriya.beans.TrainException;
+import com.supriya.constant.ResponseCode;
 
 public class ErrorHandlerServlet extends HttpServlet {
 
