db397c49f904ce39fadd12c434bfd8e13f0efedd
Supriya Pandey <113895235+supriyapandey15@users.noreply.github.com>
2026-09-07 12:42:30 +0530

    Refactor AdminLogin servlet package and imports

--- AdminLogin.java
+++ AdminLogin.java
@@ -1,4 +1,4 @@
-package com.shashi.servlets;
+package com.supriya.servlets;
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -10,9 +10,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.shashi.beans.TrainException;
-import com.shashi.constant.UserRole;
-import com.shashi.utility.TrainUtil;
+import com.supriya.beans.TrainException;
+import com.supriya.constant.UserRole;
+import com.supriya.utility.TrainUtil;
 
 @SuppressWarnings("serial")
 @WebServlet("/adminlogin")
