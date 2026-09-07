a4428f38b59275abf18783a910ea74d4829e74f4
Supriya Pandey <113895235+supriyapandey15@users.noreply.github.com>
2026-09-07 12:46:07 +0530

    Refactor AdminAddTrain servlet package and imports

--- AdminAddTrain.java
+++ AdminAddTrain.java
@@ -1,4 +1,4 @@
-package com.shashi.servlets;
+package com.supriya.servlets;
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -10,13 +10,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.shashi.beans.TrainBean;
-import com.shashi.beans.TrainException;
-import com.shashi.constant.ResponseCode;
-import com.shashi.constant.UserRole;
-import com.shashi.service.TrainService;
-import com.shashi.service.impl.TrainServiceImpl;
-import com.shashi.utility.TrainUtil;
+import com.supriya.beans.TrainBean;
+import com.supriya.beans.TrainException;
+import com.supriya.constant.ResponseCode;
+import com.supriya.constant.UserRole;
+import com.supriya.service.TrainService;
+import com.supriya.service.impl.TrainServiceImpl;
+import com.supriya.utility.TrainUtil;
 
 @WebServlet("/adminaddtrain")
 public class AdminAddTrain extends HttpServlet {
